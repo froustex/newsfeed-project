78ce29fd8ce251277717cd4958179321d00cef7f
froustex <stephane-feroussier_student2022@wilder.school>
2024-03-27 09:50:21 +0100

    base avant fix css et responsive

--- 03_JS/script.js
+++ 03_JS/script.js
@@ -54,4 +54,267 @@ likeIcon.addEventListener("click", function (event) {
   likeIcon.classList.toggle("active");
 });
 
-// News Articles Functions //
+/**************************************/
+// Inject favorites array //
+
+const container = document.querySelector(".favoris_players");
+container.style.width = "2630px";
+
+container.addEventListener("mouseover", () => {
+  container.classList.remove("active");
+});
+container.addEventListener("mouseleave", () => {
+  container.classList.add("active");
+});
+
+const favorites = [
+  {
+    id: 1,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 2,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 3,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 4,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 5,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 6,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 7,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 8,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 9,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 10,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 11,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 12,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 13,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 14,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 15,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 16,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 17,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 18,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 19,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 20,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 21,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 22,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 23,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 24,
+    photo: "01_Images/favori4.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 25,
+    photo: "01_Images/favori1.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 26,
+    photo: "01_Images/favori2.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+  {
+    id: 27,
+    photo: "01_Images/favori3.jpg",
+    nom: "Doe",
+    prenom: "John",
+  },
+];
+
+// Loop favorites array //
+for (let item of favorites) {
+  const cardsContainer = document.createElement("div");
+  cardsContainer.classList.add("favorite-container");
+  container.appendChild(cardsContainer);
+
+  const details = document.createElement("div");
+  details.classList.add("favorite-details");
+
+  cardsContainer.appendChild(details);
+
+  const favoriteImg = document.createElement("img");
+  favoriteImg.classList.add("favorite-img");
+  favoriteImg.src = `${item.photo}`;
+  const point = document.createElement("p");
+  point.classList.add("signalPoint");
+  const favoriteName = document.createElement("p");
+  favoriteName.classList.add("favorite-name");
+  favoriteName.innerHTML = `${item.nom}  ${item.prenom}`;
+  details.append(favoriteImg, point, favoriteName);
+}
+
+/**************************************/
+// Add new favorite //
+
+const newFavorite = {
+  photo: "./images/img1.jpg",
+  nom: "Ibrahim",
+  prenom: "Platini",
+};
+favorites.unshift(newFavorite);
+
+/**************************************/
+// Add dynamic posts //
+
+/*form.onsubmit = (event) => {
+  event.preventDefault();
+  const test = document.querySelector(".test");
+  const paraf = document.createElement("p");
+  paraf.innerText = `${postTitle.value}`;
+  test.append(paraf);
+
+  postTitle.value = "";
+};*/
+
+// gestion messages de confirmation intéraction boutons //
+
+const commentsButton = document.querySelector(".comment-button");
+const shareButton = document.querySelector(".share-button");
+const divMessage = document.querySelector(".interaction");
+const mess = document.createElement("p");
+
+mess.style.color = "green";
+divMessage.append(mess);
+
+// point d'avertissement clignotant + transition message de confirmation + vue message d'intéraction //
+
+const signal = document.querySelectorAll(".signalPoint");
+
+commentsButton.addEventListener("click", () => {
+  divMessage.classList.toggle("showInteraction");
+  mess.innerHTML = "votre commentaire a été enregistré";
+  const clignote = setInterval(() => {
+    signal[6].classList.toggle("signalView");
+  }, 500);
+  signal[6].addEventListener("click", () => {
+    divMessage.classList.toggle("showInteraction");
+    mess.innerHTML = "Marvin a commenté votre post";
+    clearInterval(clignote);
+    signal[6].classList.remove("signalView");
+  });
+});
+
+shareButton.addEventListener("click", () => {
+  divMessage.classList.toggle("showInteraction");
+  mess.innerHTML = "Vous venez de partager le post";
+  const clignote = setInterval(() => {
+    signal[8].classList.toggle("signalView");
+  }, 500);
+  signal[8].addEventListener("click", () => {
+    divMessage.classList.toggle("showInteraction");
+    mess.innerHTML = "Irem vient de partager votre post";
+    clearInterval(clignote);
+    signal[8].classList.remove("signalView");
+  });
+});
